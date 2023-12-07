830abef49762475beb1dfad8c0cf2e39d2b34ef6
Yaowei Lyu <32129826+Heltion@users.noreply.github.com>
2023-12-08 02:46:54 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <class T1, class T2> string to_string(pair<T1, T2>);
-template <class... T> string to_string(tuple<T...>);
+template <class T, size_t size = tuple_size<T>::value> string to_string(T);
 string to_string(char c) { return "\'"s + c + "\'"; }
 string to_string(string s) { return "\"" + s + "\""; }
 string to_string(const char* s) { return "\""s + s + "\""; }
 string to_string(bool b) { return b ? "true" : "false"; }
 string to_string(ranges::range auto r) {
-  string res = "[";
-  for (auto x : r) { res += (res.size() == 1 ? "" : ", ") + to_string(x); }
-  return res + "]";
+  string s;
+  for (auto x : r) { s += (s.empty() ? "" : ", ") + to_string(x); }
+  return "[" + s + "]";
 }
-template <class T1, class T2> string to_string(pair<T1, T2> p) {
-  return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
-template <class... T> string to_string(tuple<T...> t) {
-  string res = "(";
-  [&]<size_t... I>(index_sequence<I...>) {
-    (..., (res += (res.size() == 1 ? "" : ", ") + to_string(get<I>(t))));
-  }(make_index_sequence<sizeof...(T)>());
-  return res + ")";
+template <class T, size_t size> string to_string(T t) {
+  string s;
+  [&]<size_t... I>(index_sequence<I...>) { (..., (s += (s.empty() ? "" : ", ") + to_string(get<I>(t)))); }(make_index_sequence<size>());
+  return "(" + s + ")";
 }
 void debug_f() { cerr << ")\n"; }
 void debug_f(auto first, auto... append) {
