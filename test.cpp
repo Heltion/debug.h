8e228afaf847b1abc459dc9a928d66c72bf54ac1
Yaowei Lyu <32129826+Heltion@users.noreply.github.com>
2023-12-18 12:36:46 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,11 +9,14 @@ using namespace std;
 #endif
 
 int main() {
+  debug(vector<int>{});
   debug(1, 1 + 1, 1.0, 1.0 / 2, 'a', "a", "a"s, true);
   debug(vector({3, 1, 4, 1, 5}));
   debug(pair(1, 2.0));
   debug(pair(pair(1, "a"), pair(3, 4 / 2.)));
   debug(tuple(3, 4, 5));
   debug(pair(tuple(3, 2, 4, pair(1, 2)), vector({pair(1, 2), pair(3, 4), pair(5, 6)})));
+  debug(vector<bool>{true, false, true});
+  debug(bitset<10>(12));
   debug(new int());
 }
