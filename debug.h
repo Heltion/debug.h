aa6db96b13bbbae21040deda2fd2fcd8b2b83567
Yaowei Lyu <32129826+Heltion@users.noreply.github.com>
2023-12-18 11:52:46 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -4,6 +4,7 @@ template <class T, size_t size = tuple_size<T>::value> string to_string(T);
 string to_string(char c) { return "\'"s + c + "\'"; }
 string to_string(string s) { return "\"" + s + "\""; }
 string to_string(const char* s) { return "\""s + s + "\""; }
+string to_string(auto* p) { return to_string(size_t(p)); }
 string to_string(bool b) { return b ? "true" : "false"; }
 string to_string(ranges::range auto r) {
   string s;
@@ -15,18 +16,4 @@ template <class T, size_t size> string to_string(T t) {
   [&]<size_t... I>(index_sequence<I...>) { ((s += ", " + to_string(get<I>(t))), ...); }(make_index_sequence<size>());
   return "(" + s.substr(s.empty() ? 0 : 2) + ")";
 }
-void debug_f() { cerr << ")\n"; }
-void debug_f(auto first, auto... append) {
-  cerr << to_string(first);
-  if (sizeof...(append)) { cerr << ", "; }
-  debug_f(append...);
-}
-#define debug(x...)                                     \
-  cerr << __FILE__ ":" << __LINE__ << ": (" #x ") = ("; \
-  debug_f(x)
-#define debug_assert(a, x...)                                                        \
-  if (not(a)) {                                                                      \
-    cerr << __FILE__ ":" << __LINE__ << ": Assertion `" #a "` failed. (" #x ") = ("; \
-    debug_f(x);                                                                      \
-    exit(0);                                                                         \
-  }
+#define debug(x...) cerr << __FILE__ ":" << __LINE__ << ": (" #x ") = " << to_string(tuple(x)) << "\n"
