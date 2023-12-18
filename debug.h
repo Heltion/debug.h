1e17e947fcb708f6d42bfee248f0a0ca8140cb25
Yaowei Lyu <32129826+Heltion@users.noreply.github.com>
2023-12-18 12:36:30 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,19 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <class T, size_t size = tuple_size<T>::value> string to_string(T);
-string to_string(char c) { return "\'"s + c + "\'"; }
-string to_string(string s) { return "\"" + s + "\""; }
-string to_string(const char* s) { return "\""s + s + "\""; }
-string to_string(auto* p) { return to_string(size_t(p)); }
-string to_string(bool b) { return b ? "true" : "false"; }
-string to_string(ranges::range auto r) {
+template <class T> concept printable = requires(ostream& os, T t) { os << t; } and not is_same_v<T, string>;
+template <class T, size_t size = tuple_size<T>::value> string to_debug(T);
+string to_debug(printable auto t) {
+  ostringstream oss;
+  oss << t;
+  return oss.str();
+}
+string to_debug(ranges::range auto r) {
   string s;
-  for (auto x : r) { s += ", " + to_string(x); }
+  for (auto x : r) { s += ", " + to_debug(x); }
   return "[" + s.substr(s.empty() ? 0 : 2) + "]";
 }
-template <class T, size_t size> string to_string(T t) {
+template <class T, size_t size> string to_debug(T t) {
   string s;
-  [&]<size_t... I>(index_sequence<I...>) { ((s += ", " + to_string(get<I>(t))), ...); }(make_index_sequence<size>());
+  [&]<size_t... I>(index_sequence<I...>) { ((s += ", " + to_debug(get<I>(t))), ...); }(make_index_sequence<size>());
   return "(" + s.substr(s.empty() ? 0 : 2) + ")";
 }
-#define debug(x...) cerr << __FILE__ ":" << __LINE__ << ": (" #x ") = " << to_string(tuple(x)) << "\n"
+#define debug(x...) cerr << __FILE__ ":" << __LINE__ << ": (" #x ") = " << to_debug(tuple(x)) << "\n"
