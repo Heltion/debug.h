de6a4d0d5a3d6acebc2f53a77bc7e9203145e32a
Yaowei Lyu <32129826+Heltion@users.noreply.github.com>
2023-12-18 11:53:03 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "debug.h"
+
+#ifndef ONLINE_JUDGE
+#include "debug.h"  // https://github.com/Heltion/debug.h/blob/main/debug.h
+#else
+#define debug(...) 42
+#define debug_assert(...) 42
+#endif
+
 int main() {
   debug(1, 1 + 1, 1.0, 1.0 / 2, 'a', "a", "a"s, true);
   debug(vector({3, 1, 4, 1, 5}));
   debug(pair(1, 2.0));
   debug(pair(pair(1, "a"), pair(3, 4 / 2.)));
   debug(tuple(3, 4, 5));
-  debug(pair(tuple(3, 2, 4, pair(1, 2)),
-             vector({pair(1, 2), pair(3, 4), pair(5, 6)})));
+  debug(pair(tuple(3, 2, 4, pair(1, 2)), vector({pair(1, 2), pair(3, 4), pair(5, 6)})));
+  debug(new int());
 }
