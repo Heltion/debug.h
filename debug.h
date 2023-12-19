487468f77aa4a4100d097d68f6f3c49050d843dd
Yaowei Lyu <32129826+Heltion@users.noreply.github.com>
2023-12-19 16:54:16 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,15 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-template <class T> concept printable = requires(ostream& os, T x) { os << x; };
-template <class T> concept iterable = ranges::range<T> and not is_same_v<T, string> and not requires() { tuple_size<T>::value; };
 template <class T, size_t size = tuple_size<T>::value> string to_debug(T, string s = "");
-string to_debug(printable auto x) { return static_cast<ostringstream>(ostringstream() << x).str(); }
-string to_debug(iterable auto x, string s = "") {
+string to_debug(auto x) requires requires(ostream& os) { os << x; } { return static_cast<ostringstream>(ostringstream() << x).str(); }
+string to_debug(ranges::range auto x, string s = "") requires(not is_same_v<decltype(x), string> and not requires { tuple_size<decltype(x)>::value; }) {
   for (auto xi : x) { s += ", " + to_debug(xi); }
   return "[" + s.substr(s.empty() ? 0 : 2) + "]";
 }
-template <class T, size_t size> string to_debug(T t, string s) {
-  [&]<size_t... I>(index_sequence<I...>) { ((s += ", " + to_debug(get<I>(t))), ...); }(make_index_sequence<size>());
+template <class T, size_t size> string to_debug(T x, string s) {
+  [&]<size_t... I>(index_sequence<I...>) { ((s += ", " + to_debug(get<I>(x))), ...); }(make_index_sequence<size>());
   return "(" + s.substr(s.empty() ? 0 : 2) + ")";
 }
 #define debug(...) cerr << __FILE__ ":" << __LINE__ << ": (" #__VA_ARGS__ ") = " << to_debug(tuple(__VA_ARGS__)) << "\n"
